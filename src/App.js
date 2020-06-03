95215e5185f20d116cea188e07cef29dea2fa93d
dsaeun <daeun84366@gmail.com>
2020-06-04 00:13:07 +0900

    loginform and louter, component setting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import AdminLogin from './page/AdminLogin';
+import Header from './component/Header';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <div>
+          <Header/>       
+          <Switch>
+            <Route exact path="/"><h1 className="adminTitle">이 페이지는 관리자 전용 페이지입니다. 로그인을 하세요.</h1></Route>
+            <Route path="/AdminLogin" component={AdminLogin}></Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
