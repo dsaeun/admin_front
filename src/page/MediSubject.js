cd9f28740673f50308da399294bd380b64a22eac
dsaeun <daeun84366@gmail.com>
2020-09-16 23:45:33 +0900

    add medi, part, sym page component

---
+++ src/page/MediSubject.js
@@ -0,0 +1,10 @@
+import React from "react";
+import '../App.css';
+
+let MediSubject = () => {
+    return(
+        <div><h1>hdafdsfsi</h1></div>
+    )
+}
+
+export default MediSubject;
---
+++ src/page/PartManage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import '../App.css';
+
+let PartManage = () => {
+    return(
+        <div><h1>hdafdsfsi</h1></div>
+    )
+}
+
+export default PartManage;
